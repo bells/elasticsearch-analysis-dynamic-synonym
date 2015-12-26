3ba3d86040342ddf1099f981a09d10658e60ec14
bells <bellszhu@gmail.com>
2015-12-26 15:43:44 +0800

    补充插件资源文件

---
+++ src/main/java/com/bellszhu/elasticsearch/plugin/DynamicSynonymPlugin.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package com.bellszhu.elasticsearch.plugin;
+
+import org.elasticsearch.common.inject.Module;
+import org.elasticsearch.index.analysis.AnalysisModule;
+import org.elasticsearch.plugins.AbstractPlugin;
+
+import com.bellszhu.elasticsearch.plugin.synonym.index.analysis.DynamicSynonymTokenFilterFactory;
+
+/**
+ * @author bellszhu
+ *
+ */
+public class DynamicSynonymPlugin extends AbstractPlugin {
+
+	@Override
+	public String description() {
+		return "Analysis-plugin for synonym";
+	}
+
+	@Override
+	public String name() {
+		return "dynamic synonym";
+	}
+
+	@Override
+	public void processModule(Module module) {
+		if (module instanceof AnalysisModule) {
+			AnalysisModule analysisModule = (AnalysisModule) module;
+			analysisModule.addTokenFilter("dynamic_synonym",
+					DynamicSynonymTokenFilterFactory.class);
+		}
+	}
+
+}
